013caf3b6c02e27e90065e32ac007a40b3fb97b2
Serhii Kryhin <55597550+HeHelios@users.noreply.github.com>
2020-07-28 23:23:58 +0300

    Graphics core
    Added functions that initialize and update window with spin grid.

--- graphics_core.py
+++ graphics_core.py
@@ -1,8 +1,9 @@
 from math import floor
 from tkinter import *
+from Grid import Field
 
 Na = 0
-Na = 0
+Nb = 0
 a = 0
 c = 0
 root = 0
@@ -21,27 +22,33 @@ def init(x, y, width = 800, height = 800, bg='white'):
 def grid():  #grid for spins
     
     for i in range(0,Na+1):
-        c.create_line(50, 10+i*a, 750, 10+i*a) 
+        c.create_line(50+i*a, 50 , 50+i*a, 50 + Nb*a) 
 
     for i in range(0,Nb+1):
-        c.create_line(50+i*a, 10 , 50+i*a, 710)
+        c.create_line(50, 50+i*a, 50 + Na * a, 50+i*a)
 
 
 def spins_plot(grid): #spin sectangles
 
     for x, y in grid:
-            if grid.GetCell(x, y) == -1:
-                c.create_rectangle(51+x*a, 11+y*a, 64+x*a , 24+y*a, fill='red', outline='red',width=0)
+        if grid.GetCell(x, y) == -1:
+            col = 'red'
+        else:     
+            col = '#28F'
+                
+        c.create_rectangle(51+x*a, 51+y*a, 51+(x+1)*a , 51+(y+1)*a, fill=col, outline=col ,width=0)
 
-            if grid.GetCell(x, y) == 1:
-                c.create_rectangle(51+x*a, 11+y*a, 64+x*a , 24+y*a, fill='#28F', outline='blue',width=0)
 
-
-def latt():    #redraw of the screen
+def latt(g):    #redraw of the screen
     c.delete(ALL)
-    spins_plot()
+    spins_plot(g)
     grid()
     c.update()
 
 if __name__ == '__main__':
-    pass
+    x = 20
+    y = 10
+    g = Field(x, y)
+    g.SpinFlip(2,1)
+    init(x, y)
+    latt(g)
